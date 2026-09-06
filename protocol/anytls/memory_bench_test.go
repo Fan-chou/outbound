ff65db4eb6328fa045a42e6114aab0fad46bf327
Fan-chou <zrfchou@qq.com>
2026-09-06 10:04:44 +0800

    test(anytls): compare relay chunk sizes with TLS

--- protocol/anytls/memory_bench_test.go
+++ protocol/anytls/memory_bench_test.go
@@ -1,7 +1,12 @@
 package anytls
 
 import (
+	"crypto/tls"
+	"fmt"
 	"github.com/daeuniverse/outbound/protocol/infra/bench"
+	"io"
+	"net"
+	"net/http/httptest"
 	"runtime"
 	"testing"
 )
@@ -33,5 +38,64 @@ func BenchmarkWaitingSessionMemory(b *testing.B) {
 		for _, s := range sessions {
 			_ = s.Close()
 		}
+	}
+}
+
+func BenchmarkRelayChunkSize(b *testing.B) {
+	for _, size := range []int{8192, 32768} {
+		b.Run(fmt.Sprint(size), func(b *testing.B) {
+			s := newSession(bench.NewNetDiscardConn(), 0)
+			defer s.Close()
+			st, err := s.newStream("127.0.0.1:80")
+			if err != nil {
+				b.Fatal(err)
+			}
+			payload := make([]byte, size)
+			b.SetBytes(1 << 20)
+			b.ReportAllocs()
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				for n := 0; n < 1<<20; n += size {
+					if _, err := st.Write(payload); err != nil {
+						b.Fatal(err)
+					}
+				}
+			}
+		})
+	}
+}
+
+func BenchmarkRelayTLSChunkSize(b *testing.B) {
+	certServer := httptest.NewTLSServer(nil)
+	certs := certServer.TLS.Certificates
+	certServer.Close()
+	for _, size := range []int{8192, 32768} {
+		b.Run(fmt.Sprint(size), func(b *testing.B) {
+			a, z := net.Pipe()
+			server := tls.Server(z, &tls.Config{Certificates: certs})
+			done := make(chan struct{})
+			go func() { defer close(done); _, _ = io.Copy(io.Discard, server) }()
+			client := tls.Client(a, &tls.Config{InsecureSkipVerify: true}) // in-memory benchmark certificate
+			if err := client.Handshake(); err != nil {
+				b.Fatal(err)
+			}
+			defer func() { _ = a.Close(); _ = z.Close(); <-done }()
+			s := newSession(client, 0)
+			st, err := s.newStream("127.0.0.1:80")
+			if err != nil {
+				b.Fatal(err)
+			}
+			payload := make([]byte, size)
+			b.SetBytes(1 << 20)
+			b.ReportAllocs()
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				for n := 0; n < 1<<20; n += size {
+					if _, err := st.Write(payload); err != nil {
+						b.Fatal(err)
+					}
+				}
+			}
+		})
 	}
 }
